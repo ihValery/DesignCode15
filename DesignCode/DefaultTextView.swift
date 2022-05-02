a6b8f19ec03e320b55f46fc1b9eb318c21a54396
ihValery <ihValery@gmail.com>
2022-05-03 01:00:17 +0300

    Finish

---
+++ DesignCode/DefaultTextView.swift
@@ -0,0 +1,50 @@
+//
+//  DefaultTextView.swift
+//  DesignCode
+//
+//  Created by Валерий Игнатьев on 2.05.22.
+//
+
+import SwiftUI
+
+//MARK: - DefaultTextView
+
+struct DefaultTextView: View {
+    
+    //MARK: Properties
+    
+    private let text: String
+    
+    private let font: Font
+    
+    private let weight: Font.Weight
+    
+    private let lineLimit: Int
+    
+    //MARK: Body
+    
+    var body: some View {
+        Text(text)
+            .font(font)
+            .fontWeight(weight)
+            .lineLimit(lineLimit)
+    }
+    
+    //MARK: Initializer
+    
+    init(_ text: String, font: Font, weight: Font.Weight, lineLimit: Int = 1) {
+        self.text = text
+        self.font = font
+        self.weight = weight
+        self.lineLimit = lineLimit
+    }
+}
+
+//MARK: - PreviewProvider
+
+struct DefaultTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        DefaultTextView(GlobalConstant.Text.homeCardTitle,
+                        font: .largeTitle, weight: .bold)
+    }
+}
