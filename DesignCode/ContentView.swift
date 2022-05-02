e5eda3b9e232b8b2030407974f6f59100cfa6eaf
ihValery <ihValery@gmail.com>
2022-05-02 17:28:53 +0300

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Валерий Игнатьев on 2.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Валерий Игнатьев on 2.05.22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
