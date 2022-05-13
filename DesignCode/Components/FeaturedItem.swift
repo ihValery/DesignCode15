824a67690f409d93de0c047cf4520f5bb24e93bc
ihValery <ihValery@gmail.com>
2022-05-13 17:38:46 +0300

    FeaturedItem

---
+++ DesignCode/Components/FeaturedItem.swift
@@ -0,0 +1,97 @@
+//
+//  FeaturedItem.swift
+//  DesignCode
+//
+//  Created by Валерий Игнатьев on 13.05.22.
+//
+
+import SwiftUI
+
+//MARK: - FeaturedItem
+
+struct FeaturedItem: View {
+    
+    //MARK: Internal Constant
+    
+    struct InternalConstant {
+        static let gradientForText = LinearGradient.init(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing)
+        static let offsetBackground: CGSize = .init(width: 250, height: -100)
+        static let offsetOverlay: CGSize = .init(width: 32, height: -80)
+    }
+    
+    //MARK: Properties
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: GlobalConstant.Padding.step8) {
+            Spacer()
+            
+            Image(GlobalConstant.Logos.logo2)
+                .resizable()
+                .scaledToFit()
+                .frame(width: GlobalConstant.Size.homeCardLogo.width,
+                       height: GlobalConstant.Size.homeCardLogo.height)
+                .cornerRadius(GlobalConstant.Corner.logo)
+                .padding(GlobalConstant.Padding.step9)
+                .background(.ultraThinMaterial,
+                            in: RoundedRectangle(cornerRadius: GlobalConstant.Corner.logoBackground, style: .continuous)
+                )
+                .defaultStroke(GlobalConstant.Corner.logoBackground)
+            
+            DefaultText(GlobalConstant.Text.homeCardTitle,
+                            font: .largeTitle, weight: .bold)
+            .foregroundStyle(InternalConstant.gradientForText)
+            
+            DefaultText(GlobalConstant.Text.homeCardSubtitle.uppercased(),
+                            font: .footnote, weight: .semibold)
+            .foregroundStyle(.secondary)
+            
+            DefaultText(GlobalConstant.Text.homeCardDescription,
+                            font: .footnote, weight: .semibold, lineLimit: 2)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .foregroundColor(.secondary)
+        }
+        .padding(GlobalConstant.Padding.stepDefault)
+        .padding(.vertical, GlobalConstant.Padding.stepDefault)
+        .frame(height: GlobalConstant.Size.homeCard.height)
+        .background(.ultraThinMaterial,
+                    in: RoundedRectangle(cornerRadius: GlobalConstant.Corner.card, style: .continuous))
+        .defaultShadow()
+        .defaultStroke()
+        .padding(.horizontal, GlobalConstant.Padding.stepDefault)
+        .background(
+            GlobalConstant.Images.blobOne
+                .offset(InternalConstant.offsetBackground)
+        )
+        .overlay(
+            GlobalConstant.Images.illustration5
+                .resizable()
+                .scaledToFit()
+                .frame(height: GlobalConstant.Size.homeCardImage.height)
+                .offset(InternalConstant.offsetOverlay)
+        )
+        
+        
+        .onAppear {
+            print("Hello ContentView")
+        }
+        .onDisappear {
+            print("Bye ContentView")
+        }
+    }
+}
+
+//MARK: - PreviewProvider
+
+struct FeaturedItem_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            FeaturedItem()
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+            
+            FeaturedItem()
+                .previewLayout(.sizeThatFits)
+        }
+        .padding(.vertical, 20)
+    }
+}
